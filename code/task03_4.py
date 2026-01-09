166a6fbbe67cb863975f453b4aaf002941aaa34e
Matteo Flores s337579 <s337579@studenti.polito.it>
2026-01-09 20:14:08 +0100

    feat: task03 arch 4 code generated

---
+++ code/task03_4.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def sort_numbers(numbers: str) -> str:
+    word_to_num = {
+        'zero': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9
+    }
+    words = numbers.split()
+    sorted_words = sorted(words, key=lambda x: word_to_num[x])
+    return ' '.join(sorted_words)
