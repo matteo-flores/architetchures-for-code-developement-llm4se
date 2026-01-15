0b3c65d4976483b4f438f1f8f37c760ac37800c7
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-01-15 21:55:19 +0100

    update task06_3

--- code/task06_3.py
+++ code/task06_3.py
@@ -1,9 +1,6 @@
-from typing import List
-
 def odd_position(nums: List[int]) -> bool:
     result = True
     for i in range(len(nums)):
         if i % 2 == 1 and nums[i] % 2 == 0:
             result = False
-            break
     return result
