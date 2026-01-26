0e157954b513b37b67c87d03a1e58744210df3f9
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-01-26 11:51:39 +0100

    Update task06_1.py

--- code/task06_1.py
+++ code/task06_1.py
@@ -1,7 +1,10 @@
 from typing import List
 
+
 def odd_position(nums: List[int]) -> bool:
-    for i in range(1, len(nums), 2):
-        if nums[i] % 2 == 0:
-            return False
+    """ Write a python function to check whether every odd index contains odd numbers of a given list. """
+    for i in range(len(nums)):
+        if i % 2 != 0:  # Check if the index is odd
+            if nums[i] % 2 == 0:  # Check if the number at the odd index is even
+                return False
     return True
